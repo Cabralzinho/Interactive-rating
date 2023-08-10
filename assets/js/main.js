82e1c0cd154f7fe17621a8d688621e1e009976ea
Cabral <Cabralzinho@users.noreply.github.com>
2023-08-10 11:23:16 -0300

    ajustando mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ const selectedStar = document.getElementById("selectedStar");
 stars.forEach((star) => {
   star.addEventListener("click", () => {
     ratingValue = star.getAttribute("data-value");
-    submitRanting.textContent = `Avaliar ${ratingValue} estrela(s).`;    
     
     stars.forEach((s) => s.classList.remove("selected"));
     star.classList.add("selected");
